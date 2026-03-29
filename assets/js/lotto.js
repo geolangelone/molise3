561414544be1d02e3125b1056b4c7d4bc70d5416
geolangelone <geol.angelone@gmail.com>
2026-03-29 17:10:27 +0200

    Update lotto.js

--- assets/js/lotto.js
+++ assets/js/lotto.js
@@ -197,16 +197,19 @@
       const indaginiLayer = L.layerGroup().addTo(map);
       drawIndagini(indaginiLayer);
 
-      try {
-const boundsGroup = L.featureGroup([geoLayer, indaginiLayer]);
+   
+try {
+  const boundsGroup = L.featureGroup([geoLayer, indaginiLayer]);
 
-if (boundsGroup.getBounds().isValid()) {
-  map.fitBounds(boundsGroup.getBounds(), { padding: [20, 20] });
-} else if (geoLayer.getBounds().isValid()) {
-  map.fitBounds(geoLayer.getBounds(), { padding: [20, 20] });
-} else {
+  if (boundsGroup.getBounds().isValid()) {
+    map.fitBounds(boundsGroup.getBounds(), { padding: [20, 20] });
+  } else if (geoLayer.getBounds().isValid()) {
+    map.fitBounds(geoLayer.getBounds(), { padding: [20, 20] });
+  } else {
+    map.setView(data.center, 11);
+  }
+} catch (e) {
   map.setView(data.center, 11);
-}
         } else if (geoLayer.getBounds().isValid()) {
           map.fitBounds(geoLayer.getBounds(), { padding: [20, 20] });
         } else {
